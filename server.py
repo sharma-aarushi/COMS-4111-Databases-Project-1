d772f8b44d738cc0d349e051efe7cbd7017122f8
sharma-aarushi <s.aarushi@aol.com>
2024-11-18 18:18:00 +0000

    Change port

--- server.py
+++ server.py
@@ -730,7 +730,7 @@ if __name__ == "__main__":
     @click.option('--debug', is_flag=True)
     @click.option('--threaded', is_flag=True)
     @click.argument('HOST', default='0.0.0.0')
-    @click.argument('PORT', default=8131, type=int)
+    @click.argument('PORT', default=8141, type=int)
     def run(debug, threaded, host, port):
         HOST, PORT = host, port
         print("running on %s:%d" % (HOST, PORT))
